299e523914fded3058cfae94af3ac9bc749ae6db
Link-Zhang <Link.Zhang.0@gmail.com>
2018-03-27 12:18:52 +0800

    Init, maven, gitignore

---
+++ src/main/java/com/shch/lz/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.shch.lz;
+
+/**
+ * Created by Link at 11:59 on 3/27/18.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello, world");
+    }
+}
